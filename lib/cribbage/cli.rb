086e8d8d6d16e0462378661b0e8da8a4fb01501a
davycro <me@davecro.com>
2017-01-02 15:45:06 -0500

    updated the readme

--- lib/cribbage/cli.rb
+++ lib/cribbage/cli.rb
@@ -2,13 +2,11 @@ require 'thor'
 require 'cribbage'
 module Cribbage
   class CLI < Thor
-    desc "count START CARD_1 CARD_2 CARD_3 CARD_4", "count points in hand of five cards"
+    desc "count FLIP_CARD CARD_1 CARD_2 CARD_3 CARD_4", "count points in hand of five cards"
     def count(*cards)
 
       hand = Cribbage::Hand.new(cards)
       hand.print_score
     end
-
-    default_task :count
   end
 end
